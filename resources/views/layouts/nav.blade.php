b2bd8e68d69f23259b75dc01fb4511647ea91e08
Dinhtuan1999 <nguyendinhtuan112@gmail.com>
2021-07-15 01:14:02 +0700

    day1

---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,35 @@
+<div class="container">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="{{route('home')}}">Admin <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Quản Lý Danh Mục
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{route('danhmuc.create')}}">Thêm danh mục</a>
+                        <a class="dropdown-item" href="{{route('danhmuc.index')}}">Liệt kê danh mục</a>
+                    </div>
+                </li>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Sách Truyện
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{route('truyen.create')}}">Thêm sách truyện</a>
+                        <a class="dropdown-item" href="{{route('truyen.index')}}">Liệt kê sách truyện</a>
+                    </div>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Tìm Kiếm" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Tìm Kiếm</button>
+            </form>
+        </div>
+    </nav>
+
+</div>
